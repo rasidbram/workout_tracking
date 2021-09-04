d0af396a5616922a710b8e9547f6fe5977f2378e
rasid bram <rashidaynur.ibram@yandex.com>
2021-09-04 10:12:13 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+import requests
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GENDER = "Male"
+WEIGHT_KG = "95"
+HEIGHT_CM = "190"
+AGE = "32"
+
+APP_ID = os.getenv("NT_APP_ID")
+API_KEY = os.getenv("NT_API_KEY")
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = os.getenv("SHEET_ENDPOINT")
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(url=exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "list": {
+            # "date": today_date,
+            # "time": now_time,
+            # "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    # No Auth
+    sheet_response = requests.post(url=sheet_endpoint, json=sheet_inputs)
+#
+#     # headers = {
+#     #     "Authorization": f"Basic {os.getenv('AUTHORIZATION')}",
+#     #     "Content-Type": "application/json",
+#     # }
+#     # # Basic Auth
+#     # sheet_response = requests.post(
+#     #     sheet_endpoint,
+#     #     json=sheet_inputs,
+#     #     auth=(
+#     #         os.getenv("USERNAME"),
+#     #         os.getenv("PASSWORD"),
+#     #     ),
+#     #     headers=headers,
+#     # )
+#     #
+#     # # Bearer Token
+#     # bearer_headers = {
+#     #     "Authorization": f"Bearer {os.environ['TOKEN']}"
+#     # }
+#     # sheet_response = requests.post(
+#     #     sheet_endpoint,
+#     #     json=sheet_inputs,
+#     #     headers=bearer_headers
+#     # )
+#
+    print(sheet_response.text)
